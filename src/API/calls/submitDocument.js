e2cf0118082deed892090cc36a868cd536490b1f
“s63164“ <shobhit.saxena@hdfcbank.com>
2025-02-17 18:54:31 +0530

    make proxy changes

--- src/API/calls/submitDocument.js
+++ src/API/calls/submitDocument.js
@@ -3,7 +3,7 @@ import { transformDataForApi } from "../../helper";
 
 export const submitDocument = async (document) => {  
   const payload = document && transformDataForApi(document);
-  const endpoint = "database/insert_doc_with_qna";
+  const endpoint = "https://wealthuat.hdfcbank.com/UATEOL/API/database/insert_doc_with_qna";
 
   return apiRequest(endpoint, "POST", payload);
 };
--- src/API/calls/uploadService.js
+++ src/API/calls/uploadService.js
@@ -1,7 +1,7 @@
 import { apiRequest } from "../../Document/API";
 
 export const uploadFileService = async (fileBase64, fileName, mimeType) => {
-  const endpoint = "cloud_services/upload_object";
+  const endpoint = "https://wealthuat.hdfcbank.com/UATEOL/API/cloud_services/upload_object";
   const payload = {
     file_base64: fileBase64,
     file_name: fileName,
